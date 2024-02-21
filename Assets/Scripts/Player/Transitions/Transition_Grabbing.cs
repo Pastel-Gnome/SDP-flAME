86f1a616a0294f2df74be556afc1773cba448871
anodomani <anodomani@gmail.com>
2024-02-21 15:35:39 -0600

    shadows

--- Assets/Scripts/Player/Transitions/Transition_Grabbing.cs
+++ Assets/Scripts/Player/Transitions/Transition_Grabbing.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 
 public class Transition_Grabbing : PlayerTransition
 {
-    public Transition_Grabbing(PlayerBehaviour playerNew, PlayerState transitionToNew) : base(playerNew, transitionToNew){
+    PlayerState transitionToAlt;
+    public Transition_Grabbing(PlayerBehaviour playerNew, PlayerState transitionToNew, PlayerState transitionToAltNew) : base(playerNew, transitionToNew){
+        transitionToAlt = transitionToAltNew;
     }
 
     protected override bool Listener(){
         return player.grabbing;
+    }
+
+    public override void Transition(){
+        if(Listener()){
+            if(player.heldObject){
+                transitionToAlt.Chosen();
+                player.SetPlayerState(transitionToAlt);
+            }
+            else{
+                transitionTo.Chosen();
+                player.SetPlayerState(transitionTo);
+            }
+        }
     }
 }
