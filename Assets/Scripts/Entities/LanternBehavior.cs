72bae6ecd5c46b7ba489e0fc58d26975cd8c3084
Pastel-Gnome <89545079+Pastel-Gnome@users.noreply.github.com>
2024-02-20 22:04:27 -0600

    lantern final placement on pedestals
    - Added Pedestal prefab. When a lantern is dropped within the trigger, the lantern snaps to the pedestal and is no longer grabbable.
    
    - Added a tag for the Pedestals to aid Trigger detection
    
    - Added disabled Lantern Behavior component to the Lantern prefab. It is disabled as to not interfere with playtesting

--- Assets/Scripts/Entities/LanternBehavior.cs
+++ Assets/Scripts/Entities/LanternBehavior.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 
 public class LanternBehavior : MonoBehaviour
 {
-    private float maxLight = 5.0f;
-    public float lightLevel = 5.0f;
+    // NOTE: If the light is not working, make sure the LanternBehavior script is on, the Point Light gameobject in the lantern heirarchy is enabled, and the Point Light's light component is enabled
+
+    // If you want to turn off the light, please ensure that you disable the LanternBehavior script and disable the Point Light gameobject in the lantern heirarchy
+
+    [SerializeField] float maxLight = 5.0f;
+    public float lightLevel;
+    private bool isFinalPlace = false;
 
     private Holdable lantern;
     public Light bulb;
 
     void Start()
     {
+        //lightLevel = maxLight;
+
         lantern = GetComponent<Holdable>();
         bulb = GetComponentInChildren<Light>();
+
+        if (!bulb) { Debug.LogError("Light gameobject or component not enabled"); }
     }
 
     void FixedUpdate()
     {
-        if (lantern.holder && lightLevel < maxLight){ lightLevel += Time.fixedDeltaTime; if (lightLevel == maxLight) { Debug.Log("Lantern Light Max"); } }
-        if (!lantern.holder && lightLevel > 0) { lightLevel -= Time.fixedDeltaTime; if (lightLevel == 0) { Debug.Log("Lantern Light Zero"); } }
+        if (!isFinalPlace)
+        {
+            if (lantern.holder && lightLevel < maxLight) { lightLevel += Time.fixedDeltaTime; }
+            if (!lantern.holder && lightLevel > 0) { lightLevel -= Time.fixedDeltaTime; }
 
-        bulb.intensity = lightLevel;
+            bulb.intensity = lightLevel;
+        } else
+        {
+			if (lantern.holder && lightLevel < maxLight) { lightLevel += Time.fixedDeltaTime; if (lightLevel == maxLight) { this.enabled = false; } }
+		}
     }
+
+	private void OnTriggerStay(Collider other)
+	{
+        if (other.CompareTag("Pedestal") && lantern.holder == null && isFinalPlace == false)
+        {
+            lantern.grabbed(other.transform.parent);
+            isFinalPlace = true;
+        }
+	}
 }
