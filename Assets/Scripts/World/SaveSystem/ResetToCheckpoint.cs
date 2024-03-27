c780f602ab1f08118b61273ae9ad3197b11c4075
Pastel-Gnome <89545079+Pastel-Gnome@users.noreply.github.com>
2024-03-27 17:20:52 -0500

    Fixed Saving Bugs With Holding + Lantern Loss
    - Fixed bug where player who saves without a lantern and dies with a lantern is incorrectly respawned with a "holding" animation. (Empty hands)
    - Fixed bug where player who saves with a lantern and dies without a lantern is incorrectly respawned with an "idle" animation for the arms while still holding a lantern. (Floating lantern)
    - Added player as a possible holder for saving, and also added ability to load player holding data.
    
    - Player now is reset to the last checkpoint if they lose a lantern in a kill box.
    
    - Removed unnecessary testing gate in the Feature Showcase scene, reenabled gate that works properly.

--- Assets/Scripts/World/SaveSystem/ResetToCheckpoint.cs
+++ Assets/Scripts/World/SaveSystem/ResetToCheckpoint.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ResetToCheckpoint : MonoBehaviour
 {
+	static PlayerBehaviour playerBehaviour;
+	private void Start()
+	{
+		if (playerBehaviour == null)
+		{
+			playerBehaviour = FindFirstObjectByType<PlayerBehaviour>();
+		}
+	}
+
 	// This script is used on the "kill box", which is usually in dangerous areas like a steep drop
 	private void OnTriggerEnter(Collider other)
 	{
@@ -11,6 +20,10 @@ public class ResetToCheckpoint : MonoBehaviour
         {
 			Debug.Log("Kill box entered");
 			StartCoroutine(other.GetComponent<PlayerBehaviour>().Die()); // Change if death animation different from darkness and falling death
+		} else if (other.gameObject.layer == 7) // if the object is in the "Holdable" layer, and is therefore a lantern.
+		{
+			Debug.Log("Lantern fell into killbox.");
+			StartCoroutine(playerBehaviour.Die()); // Change if death animation different from darkness and falling death
 		}
 	}
 }
